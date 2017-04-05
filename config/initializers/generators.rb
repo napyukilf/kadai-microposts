cf64a2d8015593ac631860651ec4216f73f77f46
naoyuki <sawasawa-yu@hotmail.co.jp>
2017-04-05 12:53:14 +0000

    user login

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,6 +1,6 @@
-rails.application.config.generators do |g|
+Rails.application.config.generators do |g|
   g.stylesheets false
   g.javascripts false
   g.helper false
-  g.skip_routes ture
+  g.skip_routes true
 end
